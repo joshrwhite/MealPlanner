99ea4b498d3bf5cbc21a26370abaee5d10bb6f54
Josh White <joshuarobertwhite7@gmail.com>
2020-01-16 10:38:02 -0500

    Update quickstart.py
    A better starting point

--- quickstart.py
+++ quickstart.py
@@ -1,52 +1,76 @@
-from __future__ import print_function
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import xlwt
+import xlrd
+import random
+from os import path
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+# Open Meals.xlsx to get data for meals
+mealsBook = xlrd.open_workbook("C:\Users\jrwhite\Desktop\MealPlanner\Meals.xlsx") #Change this path to the path of your Meals.xlsx
+fullMealsSheet = mealsBook.sheet_by_name('Full Meals')
+mainDishesSheet = mealsBook.sheet_by_name('Main Dishes')
+sideDishesSheet = mealsBook.sheet_by_name('Side Dishes')
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+# List Initialization from Spreadsheet
+fullMeals = [] 
+for row_index in xrange(1, fullMealsSheet.nrows):
+	fullMeals.append(fullMealsSheet.cell(row_index, 0).value) # Get column 0
 
-    service = build('calendar', 'v3', credentials=creds)
+fullMealIngredients = [] 
+for row_index in xrange(1, fullMealsSheet.nrows):
+	fullMealIngredients.append(fullMealsSheet.cell(row_index, 1).value) # Get column 1
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+mainDishes = [] 
+for row_index in xrange(1, mainDishesSheet.nrows):
+	mainDishes.append(mainDishesSheet.cell(row_index, 0).value)
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+mainDishIngredients = [] 
+for row_index in xrange(1, mainDishesSheet.nrows):
+	mainDishIngredients.append(mainDishesSheet.cell(row_index, 1).value)
 
-if __name__ == '__main__':
-    main()
+sideDishes = [] 
+for row_index in xrange(1, sideDishesSheet.nrows):
+	sideDishes.append(sideDishesSheet.cell(row_index, 0).value)
+
+sideDishIngredients = [] 
+for row_index in xrange(1, sideDishesSheet.nrows):
+	sideDishIngredients.append(sideDishesSheet.cell(row_index, 1).value)
+
+mealNum = 10
+n=1
+
+# Welcome user and get input
+print 'Welcome to Meal Planner!'
+mealNum = input('How many meals do you want to create?  ')
+
+# Create workbook and add sheet
+book = xlwt.Workbook(encoding="utf-8")
+sh = book.add_sheet("Meals")
+sh.write(0, 0, "Meal")
+sh.write(0, 1, "Ingredients")
+
+# Get List Lengths
+fullMealNum = len(fullMeals)-1
+mainDishNum = len(mainDishes)-1
+sideDishNum = len(sideDishes)-1
+
+# Get random meals and add them to the spreadsheet
+for meals in range(mealNum):
+	# Full meal or main dish with side dish?
+	mealType = random.randint(0,1)
+	# Full meal
+	if mealType == 0:
+		randomFullMeal = random.randint(0,fullMealNum)
+		sh.write(n, 0, fullMeals[randomFullMeal])
+		sh.write(n, 1, fullMealIngredients[randomFullMeal])
+	# Main dish with side dish
+	if mealType == 1:
+		randomMainDish = random.randint(0,mainDishNum)
+		randomSideDish = random.randint(0,sideDishNum)
+		randomDish = mainDishes[randomMainDish] + " and " + sideDishes[randomSideDish]
+		randomDishIngredients = mainDishIngredients[randomMainDish] + ", " + sideDishIngredients[randomSideDish]
+		sh.write(n, 0, randomDish)
+		sh.write(n, 1, randomDishIngredients)
+	n = n+1
+
+# Save the workbook
+book.save("MealPlan.xls")
+print 'MealPlan.xls has been created with %d random meals!' %mealNum
